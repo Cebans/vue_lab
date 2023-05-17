6067f7eeae65091b745bd54d750755f0aba212f8
Cebans <julestrodriguez@uniboyaca.edu.co>
2023-05-16 22:00:11 -0500

    commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,131 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div>
+    <h1>Lista de Personas</h1>
+    <div>
+      <label>Nombre:</label>
+      <input v-model="nuevoNombre" type="text">
+    </div>
+    <div>
+      <label>Apellido:</label>
+      <input v-model="nuevoApellido" type="text">
+    </div>
+    <div>
+      <label>Edad:</label>
+      <input v-model.number="nuevaEdad" type="number">
+    </div>
+    <div>
+      <label>Cedula:</label>
+      <input v-model="nuevaCedula" type="text">
+    </div>
+    <button @click="agregarPersona">Agregar Persona</button>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(persona, index) in personas" :key="index">
+        {{ persona.nombre }} {{ persona.apellido }} - Edad: {{ persona.edad }} - Cedula: {{ persona.cedula }}
+        <button @click="eliminarPersona(index)">Eliminar</button>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      Total de personas: {{ personas.length }}
+    </div>
+    <div>
+      Personas mayores de edad: {{ contadorMayoresEdad }}
+    </div>
+    <div>
+      Personas menores de edad: {{ contadorMenoresEdad }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      personas: [],
+      nuevoNombre: "",
+      nuevoApellido: "",
+      nuevaEdad: null,
+      nuevaCedula: "",
+      contadorMayoresEdad: 0,
+      contadorMenoresEdad: 0,
+    };
+  },
+  methods: {
+    agregarPersona() {
+      const nuevaPersona = {
+        nombre: this.nuevoNombre,
+        apellido: this.nuevoApellido,
+        edad: this.nuevaEdad,
+        cedula: this.nuevaCedula,
+      };
+
+      this.personas.push(nuevaPersona);
+
+      if (nuevaPersona.edad >= 18) {
+        this.contadorMayoresEdad++;
+      } else {
+        this.contadorMenoresEdad++;
+      }
+
+      this.resetForm();
+    },
+    eliminarPersona(index) {
+      const personaEliminada = this.personas[index];
+
+      if (personaEliminada.edad >= 18) {
+        this.contadorMayoresEdad--;
+      } else {
+        this.contadorMenoresEdad--;
+      }
+
+      this.personas.splice(index, 1);
+    },
+    resetForm() {
+      this.nuevoNombre = "";
+      this.nuevoApellido = "";
+      this.nuevaEdad = null;
+      this.nuevaCedula = "";
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1 {
+  color: blue;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.form-group {
+  margin-bottom: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.personas-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-bottom: 20px;
 }
-a {
-  color: #42b983;
+
+.personas-table th,
+.personas-table td {
+  padding: 8px;
+  border: 50px solid #ddd;
 }
+
+.personas-table th {
+  background-color: black;
+}
+
+.btn-agregar,
+.btn-eliminar {
+  padding: 8px 16px;
+  border: 50px;
+  background-color: #4caf50;
+  color: #fff;
+  cursor: pointer;
+}
+
+.btn-agregar:hover,
+.btn-eliminar:hover {
+  background-color: #45a049;
+}
+
 </style>
